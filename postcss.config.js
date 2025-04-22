0898d558ea7e80c4b06b9941491d46d53f8e647c
Juan23450 <56141619+Juan23450@users.noreply.github.com>
2025-04-21 23:02:13 -0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-// postcss.config.js  (CommonJS for CRA/Webpack)
+// postcss.config.js  – CommonJS for CRA/Webpack
 module.exports = {
   plugins: {
-    tailwindcss: {},     // v3 plugin lives in the main package
+    tailwindcss: {},   // v3 plugin lives in the main package :contentReference[oaicite:4]{index=4}
     autoprefixer: {},
   },
 };
